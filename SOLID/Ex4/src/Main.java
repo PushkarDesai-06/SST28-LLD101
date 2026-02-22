553f8065459ca85b53b10986f32b121cbde2fa9f
PushkarDesai-06 <pushkarjdesai@gmail.com>
2026-02-22 16:51:29 +0530

    ex4 completed

--- SOLID/Ex4/src/Main.java
+++ SOLID/Ex4/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Hostel Fee Calculator ===");
         BookingRequest req = new BookingRequest(LegacyRoomTypes.DOUBLE, List.of(AddOn.LAUNDRY, AddOn.MESS));
-        HostelFeeCalculator calc = new HostelFeeCalculator(new FakeBookingRepo());
-        calc.process(req);
+
+        List<PricingComponent> components = BookingPricingResolver.resolve(req);
+        FeeResult fee = new HostelFeeCalculator().calculate(components);
+
+        ReceiptPrinter.print(req, fee.monthly, fee.deposit);
+
+        String bookingId = "H-" + (7000 + new Random(1).nextInt(1000));
+        new FakeBookingRepo().save(bookingId, req, fee.monthly, fee.deposit);
     }
 }
